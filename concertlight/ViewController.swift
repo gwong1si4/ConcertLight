f8338ba4e8e8dccc6c580d9b56b5531f2e02e795
Rick Yip <rick_yip@smartone.com>
2019-10-31 18:11:25 +0800

    Initial commit

--- concertlight/ViewController.swift
+++ concertlight/ViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let previewView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    let popoButton: UIButton = {
+        let view = UIButton()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.setTitle("Popo Light", for: .normal)
+        return view
+    }()
+    
     override func viewDidLoad() {
+        print("Hi")
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(previewView)
+        previewView.frame = view.bounds
+        previewView.addSubview(popoButton)
+        NSLayoutConstraint.activate([
+            popoButton.topAnchor.constraint(equalTo: previewView.bottomAnchor, constant: -100),
+            popoButton.heightAnchor.constraint(equalToConstant: 50),
+            popoButton.leadingAnchor.constraint(equalTo: previewView.trailingAnchor, constant: -80),
+            popoButton.trailingAnchor.constraint(equalTo: previewView.trailingAnchor, constant: -20)
+        ])
     }
 
 
